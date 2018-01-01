224a7811714f4baae64f16952331851c555e7ec6
Wouter Raateland <wouterraateland@gmail.com>
2018-01-01 23:21:25 +0100

    Add landing page

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,32 @@ import Link from 'next/link'
 import Header from '../components/header'
 import Hand from '../components/hand'
 
-const name = `Sigur`
-
 export default () => (
   <Page>
-    <p>{name} flashed you</p>
     <Hand />
+    <h2>Flash your friends by sharing fake links over WhatsApp and other social media.</h2>
     <Link href="/create">
-      <button>Get {name} back now</button>
+      <button>Start flashing now</button>
     </Link>
     <style jsx>{`
-      p {
-        font-size: 2em;
-        text-align: center;
+      h2 {
+        position: absolute;
+        left: 0;
+        right: 0; bottom: 4em;
       }
 
       button {
         position: absolute;
         left: 0;
         right: 0; bottom: 0;
-        
+
         display: block;
         width: 100%;
         padding: 1em;
         border: .1em solid;
         border-radius: .5em;
 
+        text-align: center;
         text-transform: uppercase;
         font-family: 'Kanit', sans-serif;
         font-weight: bold;
